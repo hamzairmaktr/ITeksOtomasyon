2f941e357672a401b9f96681210c59df71a79e91
Hamza Irmak <hamzairmak46tr@gmail.com>
2023-03-18 14:30:22 +0300

    Add project files.

---
+++ Entities/Concrete/Banka.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    //public class Banka:IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string BankaAd { get; set; }
+        public string Sube { get; set; }
+        public string Iban { get; set; }
+        public string HesapNo { get; set; }
+        public string Yetkili { get; set; }
+        public DateTime Tarih { get; set; }
+        public string HesapTuru { get; set; }
+    }
+}
---
+++ Entities/Concrete/Borc.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Borc:IEntity
+    {
+        public int Id { get; set; }
+        public int MusteriId { get; set; }
+        public decimal Tutar { get; set; }
+        public decimal KacOdendi { get; set; }
+        public decimal KacOdenecek { get; set; }
+    }
+}
